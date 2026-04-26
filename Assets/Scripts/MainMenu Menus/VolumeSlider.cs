954c616947966d12e54ec57edeb7c3b522b09064
RJones-Code <russj065@gmail.com>
2026-04-25 23:09:07 -0400

    Added some tweaks based on final feedback and added menu music with corresponding audio sliders in option menus

--- Assets/Scripts/MainMenu Menus/VolumeSlider.cs
+++ Assets/Scripts/MainMenu Menus/VolumeSlider.cs
@@ -1,27 +1,39 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Audio;
 
 public class VolumeSlider : MonoBehaviour
 {
     public Slider slider;
     public TMP_Text volumeText;
 
+    public AudioMixer mixer;
+    public string mixerParameter; // MasterVolume, MusicVolume, etc.
+    public string volumeKey;
+
     void Start()
     {
-        float volume = PlayerPrefs.GetFloat("MasterVolume", 1f);
+        float volume = PlayerPrefs.GetFloat(volumeKey, 1f);
         slider.value = volume;
         UpdateText(volume);
     }
 
     public void OnSliderChanged(float value)
     {
+        PlayerPrefs.SetFloat(volumeKey, value);
+        ApplyVolume(value);
         UpdateText(value);
+    }
+
+    void ApplyVolume(float value)
+    {
+        float dB = Mathf.Log10(Mathf.Max(value, 0.0001f)) * 20f;
+        mixer.SetFloat(mixerParameter, dB);
     }
 
     void UpdateText(float value)
     {
-        int percent = Mathf.RoundToInt(value * 100);
-        volumeText.text = "" + percent + "%";
+        volumeText.text = Mathf.RoundToInt(value * 100) + "%";
     }
 }
